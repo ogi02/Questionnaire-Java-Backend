8d7c8b1ae5e4e147f6af1e14c5659df3596b073c
Irena <irena@Irenas-MacBook-Pro.local>
2024-12-11 22:40:05 +0200

    Added SwaggerConfig.java

---
+++ src/main/java/org/tu/sofia/java/questionnaire/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package org.tu.sofia.java.questionnaire.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Questionnaire API Tips", version = "v1"))
+@SecurityScheme(
+        name = "JWTBearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class SwaggerConfig { }
