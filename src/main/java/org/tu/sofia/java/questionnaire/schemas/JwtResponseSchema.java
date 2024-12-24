737bb46268853c93aedace0a785e607b78d6bede
Ognian Baruh <ognian@baruh.net>
2024-12-24 10:04:33 +0200

    Added username to token response

--- src/main/java/org/tu/sofia/java/questionnaire/schemas/JwtResponseSchema.java
+++ src/main/java/org/tu/sofia/java/questionnaire/schemas/JwtResponseSchema.java
@@ -3,7 +3,7 @@ package org.tu.sofia.java.questionnaire.schemas;
 import java.io.Serial;
 import java.io.Serializable;
 
-public record JwtResponseSchema(String jwttoken) implements Serializable {
+public record JwtResponseSchema(String token, String username) implements Serializable {
 
     @Serial
     private static final long serialVersionUID = -8091879091924046844L;
